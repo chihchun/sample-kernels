839925a5a338e95e85cd1d2b480ada117efffd7e
John Johansen <john.johansen@canonical.com>
2017-03-17 04:18:58 -0700

    apparmor: 4.8 backport support current_time() api
    4.9 commits
    078cd8279e659989b103359bb22373cc79445bde
    3cd886666ff19e9796a519e16d94fc94f79c8a4c
    
    Signed-off-by: John Johansen <john.johansen@canonical.com>

--- security/apparmor/include/backport.h
+++ security/apparmor/include/backport.h
@@ -17,4 +17,22 @@
 #ifndef __AA_BACKPORT_H
 #define __AA_BACKPORT_H
 
+#include <linux/fs.h>
+
+/* 4.8 backport
+ * 078cd8279e659989b103359bb22373cc79445bde use current_time()
+ * and 3cd886666ff19e9796a519e16d94fc94f79c8a4c current_time() api
+ */
+static inline struct timespec current_time(struct inode *inode)
+{
+	struct timespec now = current_kernel_time();
+
+	if (unlikely(!inode->i_sb)) {
+		WARN(1, "current_time() called with uninitialized super_block in the inode");
+		return now;
+	}
+
+	return timespec_trunc(now, inode->i_sb->s_time_gran);
+}
+
 #endif /* __AA_BACKPORT_H */
